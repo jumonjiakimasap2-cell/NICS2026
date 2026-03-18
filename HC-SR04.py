ace920f2056563083d9e928057d9c67f37111746
jumonjiakimasap2-cell <jumonji.akimasa.p2@dc.tohoku.ac.jp>
2026-03-19 04:47:07 +0900

    Create HC-SR04.py

---
+++ HC-SR04.py
@@ -0,0 +1,22 @@
+TRIG = 23
+ECHO = 24
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+def get_distance():
+    GPIO.output(TRIG, False)
+    time.sleep(0.01)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        end = time.time()
+
+    duration = end - start
+    return duration * 34300 / 2  # cm
