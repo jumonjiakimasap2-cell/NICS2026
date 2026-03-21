d07ab695bf722050070b36ea1dc979d2d7d0e267
jumonjiakimasap2-cell <jumonji.akimasa.p2@dc.tohoku.ac.jp>
2026-03-21 09:43:20 +0900

    Update heating.py

--- heating.py
+++ heating.py
@@ -2,20 +2,23 @@ import RPi.GPIO as GPIO
 import time
 
 heating_wire = 17
+
 print("start")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(heating_wire, GPIO.OUT)
 
-print("fire")
+try:
+    print("fire")
+    GPIO.output(heating_wire, GPIO.HIGH)
 
-GPIO.output(heating_wire, GPIO.HIGH)
+    time.sleep(3)
 
-time.sleep(3)
+    GPIO.output(heating_wire, GPIO.LOW)
+    print("done")
 
-GPIO.output(heating_wire, GPIO.LOW)
-
-print("done")
+except KeyboardInterrupt:
+    print("stopped")
 
 finally:
     GPIO.cleanup()
